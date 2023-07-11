9e80afd2e9d75d7dc77735663e795669483591a5
marceloteclas <mdj3114@gmail.com>
2023-07-11 20:19:32 -0300

    ATIVIDADE MARCELO DE JESUS

---
+++ src/test/java/br/edu/ifba/saj/ads/poo/BancoDeUsuarios.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifba.saj.ads.poo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrador
+ */
+class BancoDeUsuarios {
+    private ArrayList<Usuario>usuarios;
+
+   public  static void addUsuario(Usuario u) {
+    }
+
+   public static void autenticar(String login, String senha) {
+    }
+    
+}
