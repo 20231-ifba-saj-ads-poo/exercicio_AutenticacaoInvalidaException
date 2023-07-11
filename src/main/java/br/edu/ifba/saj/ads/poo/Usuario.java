15f58708a17570ae0b8d321d0ef00e94b64e91cc
jorge <adm.unifacs1@hotmail.com>
2023-07-11 20:06:55 -0300

    entrega jorge

---
+++ src/main/java/br/edu/ifba/saj/ads/poo/Usuario.java
@@ -0,0 +1,11 @@
+package br.edu.ifba.saj.ads.poo;
+
+public class Usuario {
+  private String login;
+  private String senha;
+
+  public Usuario(string login, String senha){
+    this.login=login;
+  }
+
+}
